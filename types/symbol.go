b2f1fafa34994b68e2b4adf9bba0c1ef1709a266
unknown <ioncotinosu12@gmail.com>
2025-08-09 01:31:41 +0300

    symbol conversions

--- types/symbol.go
+++ types/symbol.go
@@ -1,6 +1,12 @@
 package types
 
-import "minitalk/types/core"
+import (
+	"fmt"
+	"strconv"
+
+	"minitalk/types/core"
+	"minitalk/types/errors"
+)
 
 type SymbolObject struct {
 	core.Object
@@ -15,6 +21,31 @@ func NewSymbolObject(name string) *SymbolObject {
 		}
 		return NewBoolObject(obj.String() == other.String()).Object
 	})
+	if value, ok := obj.Self.(string); ok {
+		symbolBody := value[1:]
+		if len(symbolBody) >= 2 && symbolBody[0] == '\'' && symbolBody[len(symbolBody)-1] == '\'' {
+			symbolBody = symbolBody[1 : len(symbolBody)-1]
+		}
+		if iVal, err := strconv.ParseInt(symbolBody, 10, 64); err == nil {
+			obj.Set("toInteger", iVal, ObjectConstructor)
+			obj.Set("toFloat", float64(iVal), ObjectConstructor)
+		} else {
+			obj.Set("toInteger", errors.NewValueError(fmt.Sprintf("Cannot convert %s to Integer", symbolBody)).Object)
+			obj.Set("toFloat", errors.NewValueError(fmt.Sprintf("Cannot convert %s to Float", symbolBody)).Object)
+		}
+		if bVal, err := strconv.ParseBool(symbolBody); err == nil {
+			obj.Set("toBool", bVal, ObjectConstructor)
+		} else {
+			obj.Set("toBool", errors.NewValueError(fmt.Sprintf("Cannot convert %s to Bool", symbolBody)).Object)
+		}
+		if len(symbolBody) == 1 {
+			obj.Set("toCharacter", rune(symbolBody[0]), ObjectConstructor)
+		} else {
+			obj.Set("toCharacter", errors.NewTypeError("Invalid conversion to Character").Object)
+		}
+		obj.Set("toString", symbolBody, ObjectConstructor)
+		obj.Set("toSymbol", value, SymbolConstructor)
+	}
 
 	return &SymbolObject{*obj}
 }
